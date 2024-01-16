343f3de0f7ccb4394ff74bce322d727845923ac8
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2024-01-16 16:24:48 +0200

    d

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,36 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledLabel = styled.label`
-  display: flex;
-  flex-direction: column;
-  row-gap: 10px;
-  text-align: center;
+display: flex;
+flex-direction: column;
+row-gap: 10px;
+text-align: center;
 
-  input {
-    padding: 8px;
-    margin-bottom: 10px;
-    margin-left: 10px;
-  }
+input {
+  padding: 8px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+};
+  
 `;
 
-const Filter = ({ value, onChange, contacts }) => {
-  const isDuplicateName = contacts.some(
-    contact => contact.name.toLowerCase() === value.toLowerCase()
-  );
-
-  return (
-    <StyledLabel>
-      Find contacts by name:
-      <input
-        type="text"
-        placeholder="Search contacts"
-        value={value}
-        onChange={onChange}
-        style={{ borderColor: isDuplicateName ? 'red' : 'initial' }}
-      />
-      {isDuplicateName && <p>Contact with the same name already exists!</p>}
-    </StyledLabel>
-  );
-};
+const Filter = ({ value, onChange }) => (
+  <StyledLabel>
+    Find contacts by name:
+    <input
+      type="text"
+      placeholder="Search contacts"
+      value={value}
+      onChange={onChange}
+    />
+  </StyledLabel>
+);
 
 export default Filter;
