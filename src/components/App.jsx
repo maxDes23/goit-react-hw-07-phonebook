8e7f2bc7fa2848d93fe8153dc265feaa3df3cf58
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2024-01-16 16:59:56 +0200

    dz

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,9 +41,11 @@ const App = () => {
     dispatch(setFilter(e.target.value));
   };
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredContacts = contacts
+    ? contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : [];
 
   return (
     <Container>
